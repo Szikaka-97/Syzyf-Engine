fe07ed82d7ae25095918ee813a9251bf39d806ff
BartoszNowak251597 <251597@edu.p.lodz.pl>
2026-03-29 15:13:13 +0200

    Model to mouse turn

--- src/Game_Scripts/PlayerController.h
+++ src/Game_Scripts/PlayerController.h
@@ -7,12 +7,42 @@
 #include <Camera.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
 #include <imgui.h>
 #include <cmath>
 
 class PlayerController : public GameObject, public ImGuiDrawable {
 private:
     float moveSpeed = 0.08f;
+
+    glm::vec3 GetMousePointOnGround(Camera* camera) {
+        glm::vec2 mousePos = GetScene()->Input()->GetMousePosition();
+        glm::vec2 screenSize = GetScene()->GetGraphics()->GetScreenResolution();
+
+        float x = (2.0f * mousePos.x) / screenSize.x - 1.0f;
+        float y = 1.0f - (2.0f * mousePos.y) / screenSize.y;
+
+        glm::vec4 rayStartNdc(x, y, -1.0f, 1.0f);
+        glm::vec4 rayEndNdc(x, y, 1.0f, 1.0f);
+
+        glm::mat4 invVP = glm::inverse(camera->ProjectionMatrix() * camera->ViewMatrix());
+
+        glm::vec4 rayStartWorld = invVP * rayStartNdc;
+        glm::vec4 rayEndWorld = invVP * rayEndNdc;
+
+        rayStartWorld /= rayStartWorld.w;
+        rayEndWorld /= rayEndWorld.w;
+
+        glm::vec3 rayOrigin = glm::vec3(rayStartWorld);
+        glm::vec3 rayDir = glm::normalize(glm::vec3(rayEndWorld - rayStartWorld));
+
+        if (std::abs(rayDir.y) < 0.0001f) {
+            return GlobalTransform().Position().Value();
+        }
+
+        float t = -rayOrigin.y / rayDir.y;
+        return rayOrigin + rayDir * t;
+    }
 
 public:
     void Update(){
@@ -50,8 +80,15 @@ public:
         if (glm::length(movement) > 0.0f) {
             movement = glm::normalize(movement);
             GlobalTransform().Position() += movement * moveSpeed;
+        }
 
-            float angle = std::atan2(movement.x, movement.z);
+        glm::vec3 mouseWorld = GetMousePointOnGround(camera);
+        glm::vec3 toMouse = mouseWorld - GlobalTransform().Position().Value();
+        toMouse.y = 0.0f;
+
+        if (glm::length(toMouse) > 0.001f) {
+            toMouse = glm::normalize(toMouse);
+            float angle = std::atan2(toMouse.x, toMouse.z);
             GlobalTransform().Rotation() = glm::angleAxis(angle, glm::vec3(0, 1, 0));
         }
     }
