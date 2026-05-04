d9768d268cdd189eddf938ad63211c8d987a1e2d
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-05-05 01:40:54 +0200

    refactor(editor): change editor proxy mesh back to mesh

--- editor/panels/SceneViewPanel.cpp
+++ editor/panels/SceneViewPanel.cpp
@@ -364,10 +364,11 @@ void SceneViewPanel::HandleDrop(Context& context) {
                 SceneNode* modelNode = context.selectedScene->CreateNode();
 
                 if (mesh->GetDefaultMaterials().empty()) {
-                    ShaderProgram* defaultProg = ShaderProgram::Build()
-                    .WithVertexShader(   "./res/shaders/lit.vert")
-                    .WithPixelShader("./res/shaders/lambert color.frag")
-                    .Link();
+                    ShaderProgram* defaultProg =
+                        ShaderProgram::Build()
+                            .WithVertexShader("./res/shaders/lit.vert")
+                            .WithPixelShader("./res/shaders/lambert color.frag")
+                            .Link();
 
                     auto* defaultMaterial = new Material(defaultProg);
                     defaultMaterial->SetValue("uColor", glm::vec3(0.8));
