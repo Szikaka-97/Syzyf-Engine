715b5a7f0f9a0dd4fcff2ccf51b63a2deddeca60
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-05-07 16:29:34 +0200

    fix(post-processing): fix enable

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -1588,6 +1588,9 @@ void SceneGraphics::RenderPostprocess() {
 	Texture2D* frameDepth = dynamic_cast<Texture2D*>(GetMainFramebuffer()->GetDepthTexture());
 
 	for (auto* effect : *postProcess->GetAllObjects()) {
+        if (!effect->IsEnabled())
+            continue;
+
         PostProcessParams postProcessParams;
         postProcessParams.inputTexture = dynamic_cast<Texture2D*>(ping->GetColorTexture());
         postProcessParams.outputTexture = dynamic_cast<Texture2D*>(pong->GetColorTexture());
