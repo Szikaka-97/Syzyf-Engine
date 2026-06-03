a28c025c92bbb934918ea2e3fe78870d9996ce1f
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-06-03 04:33:13 +0200

    feat(audio): init openal

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,5 @@
 #include "Application.h"
+#include "alc.h"
 #include "physics/Jolt.h"
 
 #include <Scene.h>
@@ -138,12 +139,38 @@ bool Application::InitEngine() {
     ImGui_ImplOpenGL3_Init("#version 460");
     ImGui_ImplSDL3_InitForOpenGL(window, glContext);
 
+    // Audio
+    this->audioContext = alcCreateContext(this->audioDevice, nullptr);
+    if (!this->audioContext) {
+        spdlog::error("Failed to create OpenAL context.");
+        alcCloseDevice(this->audioDevice);
+        return false;
+    }
+
+    if (!alcMakeContextCurrent(this->audioContext)) {
+        spdlog::error("Failed to make OpenAL context current.");
+        alcDestroyContext(this->audioContext);
+        alcCloseDevice(this->audioDevice);
+        return false;
+    }
+
     return true;
 }
 
 void Application::ShutdownEngine() {
     if (this->currentScene) {
         delete this->currentScene;
+    }
+
+    if (this->audioContext) {
+        alcMakeContextCurrent(nullptr);
+        alcDestroyContext(this->audioContext);
+        this->audioContext = nullptr;
+    }
+
+    if (this->audioDevice) {
+        alcCloseDevice(this->audioDevice);
+        this->audioDevice = nullptr;
     }
 
     ImGui::DestroyPlatformWindows();
