4cda8df80f7504c1709ea9ca0747c181eadb30f9
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-06-05 19:03:21 +0200

    perf(animation): refactor skeleton system

--- src/animation/SkeletonSystem.cpp
+++ src/animation/SkeletonSystem.cpp
@@ -4,21 +4,50 @@
 
 #include "Scene.h"
 
+#include <tracy/Tracy.hpp>
+
 SkeletonSystem::SkeletonSystem(Scene* scene) : GameObjectSystem<SkeletonComponent>(scene) {
   glGenBuffers(1, &this->skinningBuffer);
   spdlog::info("Skeleton system added");
 }
 
 void SkeletonSystem::OnPreUpdate() {
-  auto objects = GetScene()->FindObjectsOfType<SkeletonComponent>();
-   
   std::size_t totalJoints = 0;
-  for (auto* skeleton : objects) {
+  for (auto* skeleton : IterateObjects()) {
     totalJoints += skeleton->joints.size();
   }
 
   if (totalJoints == 0) {
     return;
+  }
+
+  if (batchedMatrices.size() < totalJoints) {
+      batchedMatrices.resize(totalJoints);
+  }
+
+  int currentOffset = 0;
+  for (auto* skeleton : IterateObjects()) {
+      skeleton->bufferOffset = currentOffset;
+
+      if (skeleton->jointMatrices.size() != skeleton->joints.size()) {
+          skeleton->jointMatrices.resize(skeleton->joints.size());
+      }
+
+      glm::mat4 inverseMeshGlobal = glm::inverse(skeleton->GetNode()->GlobalTransform().Value());
+
+      for (std::size_t i = 0; i < skeleton->joints.size(); ++i) {
+          SceneNode* jointNode = skeleton->joints[i];
+          glm::mat4 jointGlobal = jointNode->GlobalTransform().Value();
+          glm::mat4 ibm = skeleton->inverseBindMatrices[i];
+
+          glm::mat4 finalMatrix = inverseMeshGlobal * jointGlobal * ibm;
+
+          skeleton->jointMatrices[i] = finalMatrix;
+
+          batchedMatrices[currentOffset + i] = finalMatrix;
+      }
+
+      currentOffset += skeleton->joints.size();
   }
 
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, this->skinningBuffer);
@@ -29,25 +58,7 @@ void SkeletonSystem::OnPreUpdate() {
       this->currentBufferSize = requiredSize;
   }
 
-  int currentOffset = 0;
-  for (auto* skeleton : objects) {
-    skeleton->bufferOffset = currentOffset;
-    skeleton->jointMatrices.resize(skeleton->joints.size());
-
-    glm::mat4 inverseMeshGlobal = glm::inverse(skeleton->GetNode()->GlobalTransform().Value());
-
-    for (std::size_t i = 0; i < skeleton->joints.size(); ++i) {
-      SceneNode* jointNode = skeleton->joints[i];
-      glm::mat4 jointGlobal = jointNode->GlobalTransform().Value();
-      glm::mat4 ibm = skeleton->inverseBindMatrices[i];
-
-      skeleton->jointMatrices[i] = inverseMeshGlobal * jointGlobal * ibm;
-    }
-
-    glBufferSubData(GL_SHADER_STORAGE_BUFFER, currentOffset * sizeof(glm::mat4), skeleton->jointMatrices.size() * sizeof(glm::mat4), skeleton->jointMatrices.data());
-
-    currentOffset += skeleton->joints.size();
-  }
+  glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, requiredSize, batchedMatrices.data());
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
