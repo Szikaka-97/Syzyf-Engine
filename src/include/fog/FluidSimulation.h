965516bfcbef5387fdeaf40505453f85ef12a482
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-06-12 17:32:26 +0200

    chore(fog volume): comments
    - chore(fog volume): create a fog volume example scene and add the deleted fog volume there

--- src/include/fog/FluidSimulation.h
+++ src/include/fog/FluidSimulation.h
@@ -6,8 +6,11 @@
 
 class FluidSimulation : public GameObject, public ImGuiDrawable {
 public:
+    // Velocity texture resolution
     int resolution = 512;
+    // How fast the velocity texture returns to normal after being 'drawn' on by the player, higher values -> lower damping
     float damping = 0.98f;
+    // Radius of the circle the player affects, and draws with on the velocity texture
     float playerRadius = 0.05f;
     float interactionStrength = 1.0f;
 private:
@@ -20,6 +23,7 @@ public:
     FluidSimulation();
 
     void Update();
+    // Used to retrieve the velocity map which then can get assigned to the fog volume object
     Texture2D* GetVelocityMap();
 
     void DrawImGui(); 
--- src/include/fog/FogVolume.h
+++ src/include/fog/FogVolume.h
@@ -11,27 +11,46 @@ class Material;
 
 class FogVolume : public GameObject, public ImGuiDrawable {
 public:
+    // Lower values -> higher quality, worse performance
+    //  Higher values -> better performance, banding
     float stepSize = 0.1f;
+    // Makes the fog more opaque, look thicker
     float scatteringDensity = 0.7f;
+    // Makes the fog darker
     float absorptionDensity = 0.03f;
+    // Anisotropy, changes the direction of light scattering when looking at a light source
     float k = 0.005f;
+    // Stops the calculations early if the transmittance falls below this threshold, better performance the higher it is
     float transmittanceThreshold = 0.001f;
 
+    // Prevents shadow artifacts
     float bias = 0.005f;
+    // Better performance at lower values,
+    //  Needs to be higher the deeper the volume is
     unsigned int maxSteps = 64;
 
+    // Fog color
     glm::vec3 scatteringColor = glm::vec3(1.0f);
+    // Multiplies the fog color, might be used to make the fog glow
     float emissiveStrength = 0.0f;
+    // Can be used to give the fog a gradient
     Texture2D* colorRamp = nullptr;
-    
+   
+    // 3D Noise Texture, allows for non uniform fog
     Texture3D* noiseTexture = nullptr;
     float noiseScale = 0.01f;
+    // Direction (and speed) in which the noise texture moves
+    //  makes the fog appear to be moving
     glm::vec3 windDirection = { 0.0f, 0.0f, 0.0f };
 
+    // Texture created by the fluid simulation
+    //  allows for the player/enemies to affect the fog
     Texture2D* velocityTexture = nullptr;
     float velocityStrength = 2.0f;
-    
+   
+    // This gets subtracted from the 3d noise value
     float coverage = 0.45f;
+    // This multiplies the final 3d noise value
     float sharpness = 2.5;
 private:
     Mesh *mesh;
